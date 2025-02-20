35fc7bb3f4b8311e84fef7070cc414dc0e1e6a4e
Carlos Berio <carlos.a.berio@hotmail.com>
2025-02-20 01:21:46 -0500

    Added Global min val print

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
 
     int pipefd[2];
@@ -64,6 +62,9 @@ int main() {
 
                }
 
+              if (ChildMin > ParentMin) {std::cout << "Min = " << ChildMin << std::endl;}
+               else {std::cout << "Min = " << ParentMin << std::endl;}
+
                int ParentReceivedChild;
 
                if (read(pipefd[0],&ParentReceivedChild,sizeof(ParentReceivedChild)) > 0) {
@@ -75,8 +76,3 @@ int main() {
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
