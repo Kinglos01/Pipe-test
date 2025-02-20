67909de4af8f586918bd3b21e8ef3bdd7eee1c5d
Carlos Berio <132077473+Kinglos01@users.noreply.github.com>
2025-02-20 01:09:36 -0500

    Update main.cpp
    Removed awful tip comments

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
 
     int pipefd[2];
@@ -76,7 +74,3 @@ int main() {
     return 0;
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
